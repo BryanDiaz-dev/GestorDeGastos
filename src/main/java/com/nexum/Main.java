fe0742d16d55422e3b50b6244d1a68beda73c37e
BryanDiaz-dev <k4013165@gmail.com>
2025-02-19 19:56:31 -0300

    estructure

--- src/main/java/com/nexum/Main.java
+++ src/main/java/com/nexum/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("views/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
         stage.setTitle("Nexum");
